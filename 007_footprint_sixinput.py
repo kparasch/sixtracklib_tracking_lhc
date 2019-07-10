79ba4ca1fe5670d3f69dcb54535352635bd3a579
kparasch <konstantinos.paraschou@cern.ch>
2019-07-10 19:25:26 +0200

    Footprints are working

---
+++ 007_footprint_sixinput.py
@@ -0,0 +1,92 @@
+import pickle
+import pysixtrack
+import numpy as np
+import NAFFlib
+import helpers as hp
+import footprint
+import matplotlib.pyplot as plt
+
+import os
+
+# track_with = 'PySixtrack'
+# track_with = 'Sixtrack'
+track_with = 'Sixtracklib'
+#device = 'opencl:1.0'
+device = None
+
+n_turns = 100
+
+with open('line.pkl', 'rb') as fid:
+    line = pysixtrack.Line.from_dict(pickle.load(fid))
+
+with open('particle_on_CO.pkl', 'rb') as fid:
+    partCO = pickle.load(fid)
+
+with open('DpxDpy_for_footprint.pkl', 'rb') as fid:
+    temp_data = pickle.load(fid)
+
+xy_norm = temp_data['xy_norm']
+DpxDpy_wrt_CO = temp_data['DpxDpy_wrt_CO']
+
+
+if track_with == 'PySixtrack':
+
+    part = pysixtrack.Particles(**partCO)
+
+    x_tbt, px_tbt, y_tbt, py_tbt, sigma_tbt, delta_tbt = hp.track_particle_pysixtrack(
+        line, part=part, Dx_wrt_CO_m=0., Dpx_wrt_CO_rad=DpxDpy_wrt_CO[:, :, 0].flatten(),
+        Dy_wrt_CO_m=0, Dpy_wrt_CO_rad=DpxDpy_wrt_CO[:, :, 1].flatten(),
+        Dsigma_wrt_CO_m=0., Ddelta_wrt_CO=0., n_turns=n_turns, verbose=True)
+
+    info = track_with
+
+elif track_with == 'Sixtrack':
+    os.chdir('sixtrack')
+    x_tbt, px_tbt, y_tbt, py_tbt, sigma_tbt, delta_tbt = hp.track_particle_sixtrack(
+        partCO=partCO, Dx_wrt_CO_m=0., Dpx_wrt_CO_rad=DpxDpy_wrt_CO[:, :, 0].flatten(),
+        Dy_wrt_CO_m=0, Dpy_wrt_CO_rad=DpxDpy_wrt_CO[:, :, 1].flatten(),
+        Dsigma_wrt_CO_m=0., Ddelta_wrt_CO=0., n_turns=n_turns)
+    os.chdir('..')
+    info = track_with
+
+elif track_with == 'Sixtracklib':
+    x_tbt, px_tbt, y_tbt, py_tbt, sigma_tbt, delta_tbt = hp.track_particle_sixtracklib(
+        line=line, partCO=partCO, Dx_wrt_CO_m=0., Dpx_wrt_CO_rad=DpxDpy_wrt_CO[:, :, 0].flatten(),
+        Dy_wrt_CO_m=0., Dpy_wrt_CO_rad=DpxDpy_wrt_CO[:, :, 1].flatten(),
+        Dsigma_wrt_CO_m=0., Ddelta_wrt_CO=0., n_turns=n_turns, device=device)
+    info = track_with
+    if device is None:
+    	info += ' (CPU)'
+    else:
+    	info += ' (GPU %s)'%device
+else:
+    raise ValueError('What?!')
+
+n_part = x_tbt.shape[1]
+Qx = np.zeros(n_part)
+Qy = np.zeros(n_part)
+
+for i_part in range(n_part):
+    Qx[i_part] = NAFFlib.get_tune(x_tbt[:, i_part])
+    Qy[i_part] = NAFFlib.get_tune(y_tbt[:, i_part])
+
+Qxy_fp = np.zeros_like(xy_norm)
+
+Qxy_fp[:, :, 0] = np.reshape(Qx, Qxy_fp[:, :, 0].shape)
+Qxy_fp[:, :, 1] = np.reshape(Qy, Qxy_fp[:, :, 1].shape)
+
+plt.close('all')
+
+fig3 = plt.figure(3)
+axcoord = fig3.add_subplot(1, 1, 1)
+footprint.draw_footprint(xy_norm, axis_object=axcoord, linewidth = 1)
+axcoord.set_xlim(right=np.max(xy_norm[:, :, 0]))
+axcoord.set_ylim(top=np.max(xy_norm[:, :, 1]))
+
+fig4 = plt.figure(4)
+axFP = fig4.add_subplot(1, 1, 1)
+footprint.draw_footprint(Qxy_fp, axis_object=axFP, linewidth = 1)
+# axFP.set_xlim(right=np.max(Qxy_fp[:, :, 0]))
+# axFP.set_ylim(top=np.max(Qxy_fp[:, :, 1]))
+fig4.suptitle(info)
+plt.show(False)
