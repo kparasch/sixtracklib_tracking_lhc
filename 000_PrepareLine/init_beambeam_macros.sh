b0f3343d6c2d9bf3f9acde420907e28643ecc565
Konstantinos Paraschou <kparasch@cern.ch>
2019-07-18 16:11:09 +0200

    fix in beambeam macros init

--- 000_PrepareLine/init_beambeam_macros.sh
+++ 000_PrepareLine/init_beambeam_macros.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-git submodule init beambeam_macros
-git submodule update beambeam_macros
-cd beambeam_macros
+cd ..
+git submodule init 000_PrepareLine/beambeam_macros
+git submodule update 000_PrepareLine/beambeam_macros
+cd 000_PrepareLine/beambeam_macros
 make
 cd ..
