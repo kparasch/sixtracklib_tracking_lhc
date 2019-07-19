ced1d990199ff4bc159a93ab5c5ed99d953da9b9
Konstantinos Paraschou <kparasch@cern.ch>
2019-07-19 11:46:06 +0200

    Added option to store files with compression

--- myfilemanager_sixtracklib.py
+++ myfilemanager_sixtracklib.py
@@ -1,9 +1,10 @@
 import h5py
+import numpy
 
 def dict_to_h5(dict_save, filename):
     with h5py.File(filename, 'w') as fid:
         for kk in dict_save.keys():
-                fid[kk] = dict_save[kk]
+            fid[kk] = dict_save[kk]
 
 def h5_to_dict(filename):
     f = h5py.File(filename, 'r')
@@ -12,3 +13,14 @@ def h5_to_dict(filename):
         mydict[key] = f[key][()] 
     return mydict
          
+
+def dict_to_h5_gzip_compressed(dict_save, filename, compression_opts=4):
+    with h5py.File(filename, 'w') as fid:
+        for kk in dict_save.keys():
+            if isinstance(dict_save[kk], numpy.ndarray):
+                print('Compressing '+kk)
+                dset = fid.create_dataset(kk, dict_save[kk].shape, compression='gzip', compression_opts=compression_opts)
+                dset[...] = dict_save[kk]
+            else:
+                fid[kk] = dict_save[kk]
+
