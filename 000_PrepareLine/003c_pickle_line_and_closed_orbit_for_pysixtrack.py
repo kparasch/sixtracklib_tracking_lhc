f1dfcca9add2e8462ec467e9b04a480fa482c2dd
jlmalewicz <julie.malewicz@gmail.com>
2019-08-01 15:48:05 +0200

    Added particle tracking and renormalization to check closed orbit, R and W matrices

--- 000_PrepareLine/003c_pickle_line_and_closed_orbit_for_pysixtrack.py
+++ 000_PrepareLine/003c_pickle_line_and_closed_orbit_for_pysixtrack.py
@@ -40,6 +40,7 @@ twiss_table = mad.twiss()
 
 ##############
 
+# get an initial particle to use for finding the closed orbit
 def get_init_particles(part, d):
     new_part = pysixtrack.Particles()
     new_part.p0c = part.p0c
@@ -59,6 +60,7 @@ def get_init_particles(part, d):
 
     return new_part
 
+# Get the matrix R and vector m after 1 turn
 def get_R_m(part, line, d):
     init_part = get_init_particles(part, d)
     fin_part = get_init_particles(part, d)
@@ -98,8 +100,15 @@ def get_R_m(part, line, d):
 
     return R, m, part_CO
 
-# Symplectify R
-# use dragt_simplectify
+#iterate over ii turns to get a stable approximation
+def get_part_CO(part, line, d, ii):
+    for i in range(ii):
+        R, m, part = get_R_m(part, line, d)
+        i += 1
+        print ("turn %s of %s"%(i,ii))
+    return R, part
+
+# Symplectify R using Alex J. Dragt's algorithm
 def dragt_symplectify(M): 
     import scipy
     J = np.array([[0., 1., 0., 0., 0., 0.],
@@ -111,43 +120,35 @@ def dragt_symplectify(M):
     N = np.matmul(np.matmul(M , J), np.matmul(M.T, J.T))
     Q = scipy.linalg.expm(0.5 * scipy.linalg.logm(N))
     M_new = np.matmul(np.linalg.inv(Q), M)
-    print(np.linalg.det(M_new))
+#    print(np.linalg.det(M_new))
     return M_new
 
-def furman_symplectify(M):
-    J = np.array([[0., 0., 0., 1., 0., 0.],
-                 [ 0., 0., 0., 0., 1., 0.],
-                 [ 0., 0., 0., 0., 0., 1.],
-                 [-1., 0., 0., 0., 0., 0.],
-                 [ 0.,-1., 0., 0., 0., 0.],
-                 [ 0., 0.,-1., 0., 0., 0.]])
-
-    C = 0.5 * (3 + np.matmul(np.matmul(M , J), np.matmul(M.T, J)))
-    return np.matmul(C, M)
-
-def healy_symplectify(M):
-    J = np.array([[0., 1., 0., 0., 0., 0.],
-                  [-1., 0., 0., 0., 0., 0.],
-                  [ 0., 0., 0., 1., 0., 0.],
-                  [ 0., 0.,-1., 0., 0., 0.],
-                  [ 0., 0., 0., 0., 0., 1.],
-                  [ 0., 0., 0., 0.,-1., 0.]])
-
-    I = np.identity(6)
-
-    V = np.matmul(J, np.matmul(I - M, np.linalg.inv(I + M)))
-    W = (V + V.T)/2
-    if np.linalg.det(I - np.matmul(J, W)) != 0:
-        M_new = np.matmul(I + np.matmul(J, W), np.linalg.inv(I - np.matmul(J, W)))
-    else:
-        V_else = np.matmul(J, np.matmul(I + M, np.linalg.inv(I - M)))
-        W_else = (V_else + V_else.T)/2
-        M_new = -np.matmul(I + np.matmul(J, W_else), np.linalg(I - np.matmul(J, W_else)))
-    return M_new
+# re = np.zeros(6, 6)
+# re[0,:] = [twiss_table.re11[0], twiss_table.re12[0], 
+#            twiss_table.re13[0], twiss_table.re14[0],
+#            twiss_table.re15[0], twiss_table.re16[0]]
+# re[1,:] = [twiss_table.re21[0], twiss_table.re22[0],
+#            twiss_table.re23[0], twiss_table.re24[0],
+#            twiss_table.re25[0], twiss_table.re26[0]]                                                 
+# re[2,:] = [twiss_table.re31[0], twiss_table.re32[0],
+#            twiss_table.re33[0], twiss_table.re34[0], 
+#            twiss_table.re35[0], twiss_table.re36[0]]
+# 
+# re[3,:] = [twiss_table.re41[0], twiss_table.re42[0],
+#            twiss_table.re43[0], twiss_table.re44[0],
+#            twiss_table.re45[0], twiss_table.re46[0]]
+# 
+# re[4,:] = [twiss_table.re51[0], twiss_table.re52[0],
+#            twiss_table.re53[0], twiss_table.re54[0],
+#            twiss_table.re55[0], twiss_table.re56[0]]
+# 
+# re[5,:] = [twiss_table.re61[0], twiss_table.re62[0],
+#            twiss_table.re63[0], twiss_table.re64[0],
+#            twiss_table.re65[0], twiss_table.re66[0]]
 
 
 # Implement Normalization of fully coupled motion
-def normalization_fully_coupled(R):
+def get_W(R):
     M = dragt_symplectify(R)
     
     w0, v0 = np.linalg.eig(M)
@@ -163,7 +164,7 @@ def normalization_fully_coupled(R):
     w = np.array([w0[0], w0[2], w0[4]])
     mu = np.imag(np.log(w))
     
-    R = np.array([[ np.cos(mu[0]), np.sin(mu[0]), 0, 0, 0, 0],
+    R_mu = np.array([[ np.cos(mu[0]), np.sin(mu[0]), 0, 0, 0, 0],
                   [-np.sin(mu[0]), np.cos(mu[0]), 0, 0, 0, 0],
     
                   [ 0, 0, np.cos(mu[1]), np.sin(mu[1]), 0, 0],
@@ -172,11 +173,105 @@ def normalization_fully_coupled(R):
                   [ 0, 0, 0, 0, np.cos(mu[2]), np.sin(mu[2])],
                   [ 0, 0, 0, 0,-np.sin(mu[2]), np.cos(mu[2])]])
     
-    W = np.array([a[0,:], b[0,:], 
+    W = np.array([a[0,:], b[0,:],
                   a[1,:], b[1,:], 
                   a[2,:], b[2,:]]).T
 
-    return W, R
+    return W, R_mu
+
+
+def normalize(X, turns, nr_part, R):
+    W, idc = get_W(R)
+    X_norm = np.zeros([nr_part, turns, 6])
+    for n in range(turns):
+        for i in range(nr_part):
+            X_norm[i, n, :] = np.matmul(np.linalg.inv(W), X[i, n, :])
+    return X_norm
+
+def track_multi_particles(turns, nr_part, part_CO, R):
+    X = np.zeros([nr_part, turns, 6])
+    for i in range(nr_part):
+        X[i, 0, 0] = part_CO.x + i
+    X[:, 0, 1] = part_CO.px
+    X[:, 0, 2] = part_CO.y
+    X[:, 0, 3] = part_CO.py
+    X[:, 0, 4] = part_CO.zeta
+    X[:, 0, 5] = part_CO.delta
+    print("Tracking the %s particles for %s turns"%(nr_part, turns))
+    for n in range(turns - 1):
+        for i in range(nr_part):
+            X[i, n + 1,:] = np.matmul(R, X[i, n,:])
+        if n % 100 == 0:
+            print("turn %s of %s" % (n, turns))
+    
+    return X
+
+
+
+def return_plot(X, turns, nr_part, R):
+    
+    import matplotlib.pyplot as plt
+    plt.style.use("kostas")
+    
+    fig, axs = plt.subplots(2, 2)
+    
+    axs[0, 0].plot(X[0, :, 0], X[0, :, 1], '.b')
+    axs[0, 0].plot(X[1, :, 0], X[1, :, 1], '.c')
+    axs[0, 0].plot(X[2, :, 0], X[2, :, 1], '.m')
+    axs[0, 0].plot(X[3, :, 0], X[3, :, 1], '.r')
+    axs[0, 0].plot(X[4, :, 0], X[4, :, 1], '.y')
+   # plt.xlabel('$x$', axes = axs[0,0])
+   # plt.ylabel(r'$p_x$', axes = axs[0,0])
+    axs[0, 0].set_xlim([-10.3, 10.3])
+    axs[0, 0].set_ylim([-0.230, 0.230])
+    
+    axs[0, 0].set_title(r'$x, p_x$')
+
+    axs[1, 0].plot(X[0, :, 2], X[0, :, 3], '.b')
+    axs[1, 0].plot(X[1, :, 2], X[1, :, 3], '.c')
+    axs[1, 0].plot(X[2, :, 2], X[2, :, 3], '.m')
+    axs[1, 0].plot(X[3, :, 2], X[3, :, 3], '.r')
+    axs[1, 0].plot(X[4, :, 2], X[4, :, 3], '.y')
+   # plt.xlabel('$y$', axes=axs[1,0])
+   # plt.ylabel(r'$p_y$', axes=axs[1,0])
+    axs[1, 0].set_xlim([-10.3, 10.3])
+    axs[1, 0].set_ylim([-0.230, 0.230])
+
+    axs[1, 0].set_title(r'$y, p_y$')
+
+    X_norm = normalize(X, 100000, 5, R)
+    
+    axs[0, 1].plot(X_norm[0, :, 0], X_norm[0, :, 1], '.b')
+    axs[0, 1].plot(X_norm[1, :, 0], X_norm[1, :, 1], '.c')
+    axs[0, 1].plot(X_norm[2, :, 0], X_norm[2, :, 1], '.m')
+    axs[0, 1].plot(X_norm[3, :, 0], X_norm[3, :, 1], '.r')
+    axs[0, 1].plot(X_norm[4, :, 0], X_norm[4, :, 1], '.y')
+   # plt.xlabel('$x$ norm', axes = axs[0,1])
+   # plt.ylabel(r'$p_x$ norm', axes = axs[0,1])
+    axs[0, 1].set_xlim([-1.3, 1.3])
+    axs[0, 1].set_ylim([-1.3, 1.3])
+
+    axs[0, 1].set_title(r'$x, p_x$ norm')
+
+    axs[1, 1].plot(X_norm[0, :, 2], X_norm[0, :, 3], '.b')
+    axs[1, 1].plot(X_norm[1, :, 2], X_norm[1, :, 3], '.c')
+    axs[1, 1].plot(X_norm[2, :, 2], X_norm[2, :, 3], '.m')
+    axs[1, 1].plot(X_norm[3, :, 2], X_norm[3, :, 3], '.r')
+    axs[1, 1].plot(X_norm[4, :, 2], X_norm[4, :, 3], '.y')
+   # plt.xlabel('$y$ norm', axes = axs[1,1])
+   # plt.ylabel(r'$p_y norm$', axes = axs[1,1])
+    axs[1, 1].set_xlim([-1.3, 1.3])
+    axs[1, 1].set_ylim([-1.3, 1.3])
+
+    axs[1, 1].set_title(r'$y, p_y$ norm')
+
+    plt.show()
+    
+    return
+
+
+
+################
 
 #convert tau, pt to sigma,delta
 sigma_CO = beta0 * t_CO
@@ -188,7 +283,6 @@ mad_CO = [x_CO, px_CO, y_CO, py_CO, sigma_CO, delta_CO]
 part_on_CO = line.find_closed_orbit(
         guess=mad_CO, method='get_guess', p0c=p0c_eV)
 
-################
 
 print('Closed orbit at start machine:')
 print('x px y py sigma delta:')
